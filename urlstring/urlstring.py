156348e45fcc42e4fdea0e82e13d9c627a281abf
Hector <hectordm65@gmail.com>
2019-03-19 00:36:44 -0700

    cleaned up the code a bit

--- urlstring/urlstring.py
+++ urlstring/urlstring.py
@@ -3,11 +3,12 @@ def urlify(strurl):
     answer = ""
     prev = ""
     for i, string in enumerate(liststrings):
-        if string == " " and (prev == "%20" or prev == ""):  # gets rid of the front
-            liststrings[i] = ""
-        elif string == " " and prev != "%20":
-            liststrings[i] = "%20"
-            answer += "%20"
+        if string == " ":  # gets rid of the front
+            if prev == "%20" or prev == "":
+                liststrings[i] = ""
+            elif string == " " and prev != "%20":
+                liststrings[i] = "%20"
+                answer += "%20"
         else:
             answer += string
         prev = liststrings[i]
